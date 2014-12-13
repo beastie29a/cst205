96e635dc9a619a69119b0eeedcd5b5f1f12afcb5
Julia Diliberto <jdiliberto@csumb.edu>
2014-12-13 15:15:00 -0800

    folder of pictures and updated text placement

--- images.py
+++ images.py
@@ -1,47 +1,51 @@
-
 picArms = makePicture('C:\\Users\\GRAM\\Documents\\CST 205\\Final\\picArms.jpg')
 picNoArms = makePicture('C:\\Users\\GRAM\\Documents\\CST 205\\Final\\hangman.jpg')
-headX = 202
-headY = 68
 
-
-
-
-def makeGreen(pic):
+def yellowToGreen(pic):
+# Takes a picture as input
+# Makes yellow pixels green
+# Returns the picture
   pixels = getPixels(pic)
   for p in pixels:
+    if distance(getColor(p),yellow) < 150:
       r = getRed(p)
       g = getGreen(p)
-      b = getBlue(p)
-          
+      b = getBlue(p)          
       setRed(p, g * .50)
       setGreen(p, g)
       setBlue(p, g * .25)     
   return(pic)
 
 def addTextBoxes(pic,message,chosenItem):
- rect1X = 21
- rect1Y = 87
- rect2X = 21
- rect2Y = 210
- rect1Width = 160
- rect1Height = 70
- rect2Width = 160
- rect2Height = 70
- choiceMessage = 'How is a ' + chosenItem + ' going to help me?'
- mystyle = makeStyle(mono, italic + bold, 14)
- addRectFilled(pic,rect1X,rect1Y,rect1Width,rect1Height, blue)
- addTextWithStyle(pic, rect1X +2, rect1Y+10, message[0:20], mystyle, white)
- addTextWithStyle(pic, rect1X +2, rect1Y+20, message[21:41], mystyle, white)
+# Takes a picture and two strings as input
+# Add two textboxes with a message in each
+# Returns the new picture
+ rect1X = 11 # X coordinate of first rectangle
+ rect1Y = 87 # Y coordinate of first rectangle
+ rect2X = 11 # X coordinate of second rectangle
+ rect2Y = 210 # Y coordinate of second rectangle
+ rect1Width = 180 # width of first rectangle
+ rect1Height = 70 # height of first rectangle
+ rect2Width = 180 # width of first rectangle
+ rect2Height = 70 # height of first rectangle
+ choiceMessage = 'How is a ' + chosenItem # first line of second textbox message
+ choiceMessage2 = 'going to help me?' # second line of second textbox message
+ mystyle = makeStyle(mono, italic + bold, 14) # style for text in boxes
+ addRectFilled(pic,rect1X,rect1Y,rect1Width,rect1Height, red) # first textbox
+ addTextWithStyle(pic, rect1X +2, rect1Y+10, message[0:21], mystyle, white) # first line, first textbox
+ addTextWithStyle(pic, rect1X +2, rect1Y+30, message[21:42], mystyle, white) # second line, first textbox
+ addTextWithStyle(pic, rect1X +2, rect1Y+50, message[42:60], mystyle, white) # third line, first textbox
  if chosenItem != "none":
-   addRectFilled(pic,rect2X,rect2Y,rect2Width,rect2Height,blue)
-   addTextWithStyle(pic, rect2X +2, rect2Y+10 , choiceMessage[0:19], mystyle, white)
-   addTextWithStyle(pic, rect2X + 2, rect2Y+20,choiceMessage[19:29], mystyle, white)
+   addRectFilled(pic,rect2X,rect2Y,rect2Width,rect2Height,red) # second textbox
+   addTextWithStyle(pic, rect2X +2, rect2Y+10 , choiceMessage, mystyle, white) # first line, second textbox
+   addTextWithStyle(pic, rect2X + 2, rect2Y+30,choiceMessage2, mystyle, white) # second line, second textbox
  return pic
 
-def chromakey(pic, backpic):
+def chromakey(face, backpic):
+  headX = 202 # X coordinate of face
+  headY = 68 # Y coordinate of face
   newpic = makeEmptyPicture(getWidth(backpic), getHeight(backpic),white)
-  copyInto (pic,newpic,headX,headY)
+  copyInto (face,newpic,headX,headY)
   for x in range (0,getWidth(newpic)):
     for y in range (0,getHeight(newpic)):
       pix = getPixel(newpic,x,y)
@@ -51,26 +55,28 @@ def chromakey(pic, backpic):
         setColor(pix,getColor(altpix))
   return newpic
   
-# Introduction
-
-newpic = addTextBoxes(picArms,introMessage, "none")
-introMessage = "Man, you gotta help. Find things in the house to get me down."
+# Inital status
+face = makePicture('C:\\Users\\GRAM\\Documents\\CST 205\\Final\\worried.jpg')
+newpic = chromakey(face,picNoArms)
+newpic = addTextBoxes(newpic,introMessage, "none")
+introMessage = "Man, you gotta help. Go look in the house. Find some tools."
 show(newpic)
 
 def updatePicStatus(levelOfDestruction, itemChosen):
+# Updates the picture to reflect the number of mistakes made so far
   face1=makePicture('C:\\Users\\GRAM\\Documents\\CST 205\\Final\\hurt.jpg')  
-  deadMessage1 = "You want to see me pushing up daisies?"
-  face2=makePicture('C:\\Users\\GRAM\\Documents\\CST 205\\Final\\worried.jpg')
-  deadMessage2 = "Come on, Man, I'm gonna be six feet under!"
-  face3=makePicture('C:\\Users\\GRAM\\Documents\\CST 205\\Final\\shocked.jpg')
+  deadMessage1 = "Do you want to see me pushing up daisies?"
+  face2=makePicture( 'C:\\Users\\GRAM\\Documents\\CST 205\\Final\\question.jpg')
+  deadMessage2 = "Come on, I'm gonna be six feet under!"
+  face3=yellowToGreen(makePicture('C:\\Users\\GRAM\\Documents\\CST 205\\Final\\shocked.jpg'))
   deadMessage3 = "I'm about to kick the bucket here?!?"
   face4=makePicture('C:\\Users\\GRAM\\Documents\\CST 205\\Final\\sad.jpg')
   deadMessage4 = "Worm food! That's all I have to say"
   face5=makePicture('C:\\Users\\GRAM\\Documents\\CST 205\\Final\\pleading.jpg')
   deadMessage5 = "I'm fading away."
   face6=makePicture('C:\\Users\\GRAM\\Documents\\CST 205\\Final\\last cigarette.jpg')
-  deadMessage6 = "My life is passing before my eyes."
-  
+  deadMessage6 = "That's it. I'm riding the white horse."
+ 
   if levelOfDestruction == 1:
     newpic = chromakey(face1,picArms)
     newpic = addTextBoxes(newpic,deadMessage1,itemChosen)
@@ -88,7 +94,7 @@ def updatePicStatus(levelOfDestruction, itemChosen):
     newpic = addTextBoxes(newpic,deadMessage5,itemChosen)
   elif levelOfDestruction == 6:
     newpic = chromakey(face6,picArms)
-    newpic = addTextBoxes(newpic,deadMessage6,itemChosen)
+    newpic = addTextBoxes(newpic,deadMessage6,"none")
   show (newpic)
   
   
