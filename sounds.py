2fc4452d50ef14230ca0b6b392f80ece3e21eb15
Robert Contreras <robertcontreras@gmail.com>
2014-12-13 20:46:24 -0800

    Added other sound files, and setup functions for playback based on location

--- sounds.py
+++ sounds.py
@@ -1,7 +1,11 @@
 import time
 
-def playAnySound():
-  file = pickAFile()
+path = "/Users/rcontreras/code/cst205-final" # Set to root path of github repo
+
+#
+# Game play music, need to figure out way to call function in the background
+def backgroundMusic():
+  file = path + '/sound/background.wav'
   sound = makeSound(file)
   rate = getSamplingRate(sound)
   samples = getLength(sound)
@@ -11,6 +15,46 @@ def playAnySound():
   while (true):
     play(sound)
     time.sleep(length)
+
+# Functino to play sound when entering a room
+def enterRoomSound(room):
+  if room == 'front yard':
+    soundLibrary('doorclose')
+  elif room == 'living room':
+   soundLibrary('doorclose')
+  elif room == 'bedroom':
+   soundLibrary('doorclose')
+  elif room == 'kitchen':
+   soundLibrary('doorclose')
+  elif room == 'office':
+   soundLibrary('doorclose')
+  elif room == 'laundry room':
+   soundLibrary('stairs')
+  elif room == 'back yard':
+   soundLibrary('doorclose')
+  elif room == 'attic':
+   soundLibrary('stairs')
+  
+  
+# Finction to play sound when obtaining item
+def itemSound(item):
+  if item == 'ladder':
+    soundLibrary('dong')
+  elif item == 'scissors':
+    soundLibrary('plock')
+    
+def soundLibrary( sound ):
+  file = path + '/sound/' + sound + '.wav'
+  sound = makeSound(file)
+  play(sound)
+
+# Function to combine sounds and playthem at the end when freeing your friend
+def combineSounds():
+  return true
+  
+def helpMeSound():
+  soundLibrary('help')
+
   
 # 
 # Courtesy of freesound.com
